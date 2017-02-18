9126991d987e6d14776d89bb3b7dbb385fc98b49
Aria <aria.ohari@kakaocorp.com>
2017-02-18 15:30:20 +0900

    [bug fix] seed was not created because password condition was not satisfied

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Manager.create!(username: 'manager1', email: 'test@ggida.org', password: '1234')
+Manager.create!(username: 'manager1', email: 'test@ggida.org', password: '123456')
