8d62998a8aa5b8c861b415c59ead7d6ce1858f44
slevin.choi <slevin.choi@kakaocorp.com>
2017-02-25 08:39:10 +0900

    파일 경로

--- app/models/party.rb
+++ app/models/party.rb
@@ -23,7 +23,7 @@ class Party < ApplicationRecord
   
   has_attached_file :images, :styles => { :medium => "300x300>", :thumb => "100x100>" },
                     :default_url => "/images/:style/missing.png",
-                    :path => "public/upload/images/:id_:style_:filename"
+                    :path => ":rails_root/public/upload/images/:id_:style_:filename"
   validates_attachment_content_type :images, :content_type => /^image\/(jpeg|png|gif|tiff)$/
   validates_attachment_size :images, :in => 0..10.megabytes
 
